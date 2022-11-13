6de2e7b11f20f8dc5c7b3c840d1bd4172744af89
OPaslavskiy <bankir.op@gmail.com>
2022-11-13 12:44:13 +0200

    task - 3

---
+++ task-3.js
@@ -0,0 +1,21 @@
+const favouriteMovies = [
+  { director: "Christopher Nolan", name: "Interstellar" },
+  { director: "Quentin Tarantino", name: "Pulp Fiction" },
+  { director: "Christopher Nolan", name: "Inception" },
+  { director: "Christopher Nolan", name: "The Dark Knight" },
+  { director: "Martin Scorsese", name: "Taxi driver" },
+  { director: "Quentin Tarantino", name: "Inglorious Basterds" },
+  { director: "Martin Scorsese", name: "Silence" },
+];
+
+// const uniqDirectors = favouriteMovies.filter(({ director }, index) => favouriteMovies.findIndex(({ di }) => director === di) === index);
+
+const uniqDirectors = favouriteMovies.reduce((acc, { director, name }) => {
+  if (!acc[director]) acc[director] = [];
+    acc[director].push(name);
+  return acc;
+}, {});
+
+console.log(uniqDirectors);
+
+// favouriteMovies.indexOf(movie) = index;
