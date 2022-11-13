4d7d680f665300451982c14e902fd8d34b96af40
YaroslavYu <yaroslavyu801@gmail.com>
2022-11-13 12:55:24 +0200

    update task 3

--- task-3.js
+++ task-3.js
@@ -10,12 +10,19 @@ const favouriteMovies = [
 
 // const uniqDirectors = favouriteMovies.filter(({ director }, index) => favouriteMovies.findIndex(({ di }) => director === di) === index);
 
-const uniqDirectors = favouriteMovies.reduce((acc, { director, name }) => {
-  if (!acc[director]) acc[director] = [];
-    acc[director].push(name);
-  return acc;
-}, {});
+// const uniqDirectors = favouriteMovies.reduce((acc, { director, name }) => {
+//   if (!acc[director]) acc[director] = [];
+//     acc[director].push(name);
+//   return acc;
+// }, {});
 
-console.log(uniqDirectors);
+// console.log(uniqDirectors);
 
 // favouriteMovies.indexOf(movie) = index;
+
+const uniqDirectors = {};
+favouriteMovies.forEach(({ director, name }) => {
+  if (!uniqDirectors[director]) uniqDirectors[director] = [];
+  uniqDirectors[director].push(name);
+});
+console.log(uniqDirectors);
