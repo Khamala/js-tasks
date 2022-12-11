1d0ba7492a33808c59178f3a9c66e28128f3bdd1
YaroslavYu <yaroslavyu801@gmail.com>
2022-12-11 12:02:21 +0200

    task_2

--- lesson_4/02_statistic/index.js
+++ lesson_4/02_statistic/index.js
@@ -4,6 +4,7 @@
 // - кількість елементів кожного класу
 // - кількість елементів кожного тегу
 // Для роботи з класами рекомендується використовувати classList
+// nodeType
 
 function collectDOMStat(root) {
   const stat = {
@@ -12,7 +13,23 @@ function collectDOMStat(root) {
     texts: 0,
   };
 
+  const nodes = root.childNodes;
+  console.log(nodes);
+  for (const node of nodes) {
+    if (node.nodeType === 3) {
+      stat.texts += 1;
+    } else if (node.nodeType === 1) {
+      if (!stat.tags[node.tagName]) {
+        stat.tags[node.tagName] = 1;
+      } else stat.tags[node.tagName] += 1;
+      for (const classEl of node.classList) {
+        if (!stat.classes[classEl]) {
+          stat.classes[classEl] = 1;
+        } else stat.classes[classEl] += 1;
+      }
+    }
+  }
   return stat;
 }
 
-console.log(collectDOMStat(document.querySelector('.container')))
+console.log(collectDOMStat(document.querySelector(".container")));
