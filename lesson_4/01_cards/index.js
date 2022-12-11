bf1fde254d208d0816bb4e03f0dbee9f150d944e
OPaslavskiy <bankir.op@gmail.com>
2022-12-11 11:10:38 +0200

    task_01

--- lesson_4/01_cards/index.js
+++ lesson_4/01_cards/index.js
@@ -1,12 +1,17 @@
-//Необхідно додати потрібну логіку, щоб при кліку на одну з карток,вона ставала активною(у нас для цього є клас active), а всі інші ставали неактивними
-const container = document.querySelector(".container");
-const allSlides = document.querySelectorAll(".slide");
-container.addEventListener("click", onOpenImage);
-function onOpenImage(evt) {
-  if (!evt.target.classList.contains("container")) {
-    allSlides.forEach(function (item) {
-      item.classList.remove("active");
-    });
-    evt.target.classList.toggle("active");
+const findCards = document.querySelectorAll(".slide");
+
+for (let card of findCards) {
+  card.addEventListener("click", showSlide);
+}
+
+function showSlide(evt) {
+  closeSlides();
+  const currentSlide = evt.currentTarget.classList;
+  currentSlide.add("active");
+}
+
+function closeSlides() {
+  for (let card of findCards) {
+    card.classList.remove("active");
   }
 }
