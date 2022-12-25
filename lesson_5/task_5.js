6d99775a021430908f7ae50c2873c73dab35a535
YaroslavYu <yaroslavyu801@gmail.com>
2022-12-25 12:50:29 +0200

    task-5

--- lesson_5/task_5.js
+++ lesson_5/task_5.js
@@ -1,1 +1,20 @@
-/* * Функція countWithDelay приймає 3 аргумента: * 1) кількість секунд перед тим, як працює ф-ція logCount * 2) скільки разів повинна відпрацювати logCount * 3) затримка між викликами ф-ції * logCount повинна логувати кол-во викликів */ const countWithDelay = (delay = 0, times = 0, interval = 0) => { } countWithDelay(3000, 6, 1000);
+/* * Функція countWithDelay приймає 3 аргумента: * 
+1) кількість секунд перед тим, як працює ф-ція logCount * 
+2) скільки разів повинна відпрацювати logCount * 
+3) затримка між викликами ф-ції * 
+logCount повинна логувати к-сть викликів */
+
+const countWithDelay = (delay = 0, times = 0, interval = 0) => {
+  let count = 0;
+  function logCount() {
+    const intervalId = setInterval(() => {
+      count += 1;
+      console.log(count);
+      if (count === times) {
+        clearInterval(intervalId);
+      }
+    }, interval);
+  }
+  setTimeout(logCount, delay);
+};
+countWithDelay(3000, 6, 1000);
