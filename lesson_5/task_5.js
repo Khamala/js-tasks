ae7a247ea28c9bdf0b4babbf514a83763ce190ba
Volodymyr Kozak <vkozak@reenbit.com>
2022-12-25 12:05:24 +0200

    upload task 5

---
+++ lesson_5/task_5.js
@@ -0,0 +1,1 @@
+/* * Функція countWithDelay приймає 3 аргумента: * 1) кількість секунд перед тим, як працює ф-ція logCount * 2) скільки разів повинна відпрацювати logCount * 3) затримка між викликами ф-ції * logCount повинна логувати кол-во викликів */ const countWithDelay = (delay = 0, times = 0, interval = 0) => { } countWithDelay(3000, 6, 1000);
