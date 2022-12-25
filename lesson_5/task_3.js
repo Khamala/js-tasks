e42afb9d53e5a6fb58fbee6e11754625e8749608
YaroslavYu <yaroslavyu801@gmail.com>
2022-12-25 11:14:54 +0200

    task_3

--- lesson_5/task_3.js
+++ lesson_5/task_3.js
@@ -5,7 +5,30 @@
  * Якщо значення парне, вирішуй проміс та повертай "even" через 1 секунду.
  * Якщо значення не парне, вирішуй проміс та повертай "odd" через 2 секунди.
  */
+const value = prompt("введіть що-небудь");
 
 function checkValue(value) {
-
+  //   return new Promise((resolve, reject) => {
+  //     if (isNaN(value)) {
+  //       reject("its not a number");
+  //     }
+  //     if (value % 2 === 0) {
+  //       resolve(setTimeout(() => console.log("even"), 1000));
+  //     } else resolve(setTimeout(() => console.log("odd"), 2000));
+  //   });
+  return new Promise((resolve, reject) => {
+    if (isNaN(value)) {
+      reject("its not a number");
+    }
+    if (value % 2 === 0) {
+      setTimeout(() => {
+        resolve("even");
+      }, 1000);
+    } else
+      setTimeout(() => {
+        resolve("odd");
+      }, 2000);
+  });
 }
+
+checkValue(value).then(alert).catch(alert);
