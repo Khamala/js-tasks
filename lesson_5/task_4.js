5daaf721a1fa397067cfd8578dc6b7d59557e997
Sergii Karpus <karpus.sergey@gmail.com>
2022-12-25 11:59:34 +0200

    5 Task FINAL

--- lesson_5/task_4.js
+++ lesson_5/task_4.js
@@ -21,4 +21,26 @@ const startTimer = (arr) => {
    }, 500);
 }
 
+// const startTimer = (arr) => {
+//    let value = 0;
+//    let flag = 0; 
+//    const inteval = setInterval(() => {
+//       if (value < arr.length && !flag) {
+//          console.log(arr[value]);
+//          value += 1;
+//          if (value == arr.length) {
+//             flag = 1;
+//             value -= 1;
+//          }
+//       } else  if (value <= arr.length && flag) {
+//          value -= 1;
+//          console.log(arr[value]);
+//          if (value == 0) {
+//             clearInterval(inteval);
+//          }
+//       }
+
+//    }, 2000);
+// }
+
 startTimer(["a", "b", "c", "d", "e", "f"]);
