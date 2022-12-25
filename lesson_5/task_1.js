5cec90d680e0bbf519003064cb90fa7f55237d1b
Sergii Karpus <karpus.sergey@gmail.com>
2022-12-25 10:51:08 +0200

    1st, 2nd

--- lesson_5/task_1.js
+++ lesson_5/task_1.js
@@ -1,4 +1,5 @@
 // Створіть функцію, яка отримує функцію як аргумент і виконує її через 2 секунди
 
 function delayFunction(callback) {
+   setTimeout(callback, 2000);
 }
--- lesson_5/task_2.js
+++ lesson_5/task_2.js
@@ -1,6 +1,20 @@
 //Напишіть функцію, яка виводитиме числа від min до max із затримкою в 1 секунду між кожним значенням
 
 function counter(min, max) {
+   // for (let i = min; i <= max; i++){
+   //    setTimeout(() => {
+   //       console.log(i);
+   //    }, 1000*i);
+   // }
+   let number = min;
+   const interval = setInterval(() => {
+      if (number <= max) {
+         console.log(number);
+         number += 1;
+      } else {
+         clearInterval(interval);
+      }
+   }, 1000)
 
 }
 counter(1,10)
